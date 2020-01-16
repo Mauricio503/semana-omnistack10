335caabd4d19b8fdd7f85272f48fc18fe5c7670c
Mauricio503 <mauriciogspagnol@gmail.com>
2020-01-16 16:32:27 +0000

    em andamento

--- web/src/App.js
+++ web/src/App.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css'
+
 
 function App() {
-    
+    const [latitude, setLatitude] = useState('');
+    const [longitude, setLongitude] = useState('');
+
+    //useEffect é executado quando alguma informação for alterada
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+
+                setLatitude(latitude);
+                setLongitude(longitude)
+            },
+            (err) =>{
+                console.log(err);
+            },
+            {
+                timeout: 30000,
+            }
+        );
+    }, []);
+
   return (
     <div id="app">
         <aside>
             <strong>Cadastrar</strong>
             <form>
-                <div class="input-block">
+                <div className="input-block">
                     <label htmlFor="github_username">Usuario do Github</label>
                     <input name="github_username" id="github_username" required/>
                 </div>
-                <div class="input-block">
+                <div className="input-block">
                     <label htmlFor="techs">Tecnologias</label>
                     <input name="techs" id="techs" required/>
                 </div>
 
                 <div className="input-group">
-                    <div class="input-block">
+                    <div className="input-block">
                         <label htmlFor="latitude">Latitude</label>
-                        <input name="latitude" id="latitude" required/>
+                        <input type="number" name="latitude" id="latitude" required value={latitude} />
                     </div>
 
-                    <div class="input-block">
+                    <div className="input-block">
                         <label htmlFor="longitude">Longitude</label>
-                        <input name="longitude" id="longitude" required/>
+                        <input type="number" name="longitude" id="longitude" required value={longitude}/>
                     </div>
                 </div>
 
